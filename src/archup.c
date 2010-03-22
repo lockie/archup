3c22e24c024e1c56e26147af0330015fbd6f2a63
Rorschach <r0rschach@lavabit.com>
2010-03-22 19:15:00 +0100

    changed --version output

--- src/archup.c
+++ src/archup.c
@@ -39,8 +39,10 @@ int print_help(char *name)
 int print_version()
 {
         printf("archup 1.2\n\n");
-	printf("licence: GPLv3\n");
-	printf("copyright 2010 Rorschach <r0rschach@lavabit.com>\n");
+	printf("Copyright 2010 Rorschach <r0rschach@lavabit.com>\n");
+        printf("License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n");
+        printf("This is free software: you are free to change and redistribute it.\n");
+        printf("There is NO WARRANTY, to the extent permitted by law.\n");
         exit(0);
 }
 
