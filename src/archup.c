a227dca896b1bde68cd19b8c19f1524337b249e8
Rorschach <r0rschach@lavabit.com>
2010-03-29 22:28:04 +0200

    added ctype.h include

--- src/archup.c
+++ src/archup.c
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /* Prints the help. */
 int print_help(char *name)
